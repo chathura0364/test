dfe328c5c8ec3dbfd431cf9e718ee33f6a1b864d
chathura0364 <chathurayasisurucc@gmail.com>
2025-08-25 21:24:39 +0530

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const links     = navbar.querySelectorAll('a');
 const sections  = document.querySelectorAll('section.section');
 const yearEl    = document.getElementById('year');
 const typingEl  = document.querySelector('.typing-target');
+
+if (yearEl) yearEl.textContent = new Date().getFullYear();
